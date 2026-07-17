f9a9ba7436d560006563459ff134cb65f64fc09a
zetashift <zetashift@users.noreply.github.com>
2026-07-17 07:31:53 +0000

    deploy: 8777f75b4291edb74b645de6b99a9030a4ff2172

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-15e9a6db.js',
+                path_to_root + 'searchindex-5e79cf32.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
